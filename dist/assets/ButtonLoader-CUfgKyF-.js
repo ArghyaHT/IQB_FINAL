202316a09c76ad8beb33a1603fd22feeb85cb0d4
Arghya211994 <arghya@himanstech.com>
2024-11-22 15:57:24 +0530

    new dist added

---
+++ dist/assets/ButtonLoader-CUfgKyF-.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DGL6bZNG.js";const t="_btn_loader_4rc8e_1",s="_spin89345_4rc8e_1",n={btn_loader:t,spin89345:s},_=({color:r})=>o.jsx("div",{className:n.btn_loader});export{_ as B};
