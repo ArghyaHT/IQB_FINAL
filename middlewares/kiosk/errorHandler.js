1ffe7bec93eb7edaca920c6329d9caf2b2b8ed21
Arghya211994 <arghya@himanstech.com>
2024-11-23 12:00:17 +0530

    error handler for kiosk chnaged also

--- middlewares/kiosk/errorHandler.js
+++ middlewares/kiosk/errorHandler.js
@@ -1,11 +1,12 @@
 export const ErrorHandler = (err, req, res, next) => {
-    // const errStatus = err.statusCode || 500;
-    const errStatus = 500;
+    console.log("Middleware Error Hadnling");
+    const errStatus = err.statusCode || 500;
     const errMsg = err.message || 'Something went wrong';
     res.status(errStatus).json({
         success: false,
         status: errStatus,
         message: errMsg ,
-        // stack: process.env.NODE_ENV === 'development' ? err.stack : {}
+        stack: process.env.NODE_ENV === 'development' ? err.stack : {}
     })
 }
+
