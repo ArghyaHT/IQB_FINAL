a0d1ccac98be6cf2fa43b0007fe40c0f59991c0e
Arghya211994 <arghya@himanstech.com>
2024-12-12 17:51:20 +0530

    new dist added

--- dist/assets/Date-D5SsnKrL.js
+++
@@ -1,1 +0,0 @@
-import"./index-DDukCDKF.js";const o=()=>{const t=new Date,e=t.getFullYear(),a=String(t.getMonth()+1).padStart(2,"0"),n=String(t.getDate()).padStart(2,"0");return`${e}-${a}-${n}`};export{o as g};
---
+++ dist/assets/Date-DzNLgd7P.js
@@ -0,0 +1,1 @@
+import"./index-AHqq0E29.js";const o=()=>{const t=new Date,e=t.getFullYear(),a=String(t.getMonth()+1).padStart(2,"0"),n=String(t.getDate()).padStart(2,"0");return`${e}-${a}-${n}`};export{o as g};
