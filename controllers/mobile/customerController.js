d23e19f6b5105d4adcacd5d0a3fe88984bee106e
Arghya211994 <arghya@himanstech.com>
2025-01-06 12:17:35 +0530

    duplicate code removed

--- controllers/mobile/customerController.js
+++ controllers/mobile/customerController.js
@@ -24,7 +24,6 @@ import { SuccessHandler } from "../../middlewares/SuccessHandler.js";
 import { SALON_NOT_FOUND_ERROR, SALONS_RETRIEVED_SUCESS } from "../../constants/web/SalonConstants.js";
 import { CUSTOMER_CHANGE_DEFAULT_SALON_SUCCESS, CUSTOMER_DASHBOARD_SUCCESS } from "../../constants/mobile/SalonConstants.js";
 import { ALLOWED_IMAGE_EXTENSIONS, CUSTOMER_IMAGE_EMPTY_ERROR, IMAGE_FAILED_DELETE, IMAGE_UPLOAD_FAILED_ERROR, MAX_FILE_SIZE } from "../../constants/web/Common/ImageConstant.js";
-import { SuccessHandler } from "../../middlewares/SuccessHandler.js";
 
 
 //DESC:CHECK WEATHER THE EMAIL ALREADY EXISTS IN THE DATABASE =======
