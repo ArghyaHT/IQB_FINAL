ce5e5e3f31a2af470b159ab99357d377a3e9d9d7
Arghya211994 <arghya@himanstech.com>
2025-01-06 13:37:35 +0530

    commented code removed

--- controllers/web/appointments/appointmentHistoryController.js
+++ controllers/web/appointments/appointmentHistoryController.js
@@ -8,24 +8,12 @@ export const getAppointmentHistorybyCustomerEmail = async (req, res, next) => {
     try {
         const { salonId, customerEmail } = req.body;
 
-        // Query the AppointmentHistory collection and use $elemMatch to get only the matching appointments
         const history = await getAppointmentsByCustomerEmail(salonId, customerEmail)
 
         if (!history) {
-            // return res.status(201).json({
-            //     success: false,
-            //     message: "Appointment history not found for the given customer."
-            // });
             return ErrorHandler(APPOINTMENTS_HISTORY_NOT_FOUND_ERROR, ERROR_STATUS_CODE_201, res)
-
         }
         // Return the found history
-        // res.status(200).json({
-        //     success: false,
-        //     message: "Appointment history retrieved successfully",
-        //     response: history
-        // });
-
         return SuccessHandler(APPOINTMENTS_HISTORY_RETRIEVE_SUCCESS, SUCCESS_STATUS_CODE, res, { response: history });
 
     } catch (error) {
