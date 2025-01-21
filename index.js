e1a490a1890cf4257f1314174ff2e64d2f74947c
Arghya211994 <arghya@himanstech.com>
2025-01-21 16:57:11 +0530

    expiry date check

--- index.js
+++ index.js
@@ -218,15 +218,24 @@ app.post('/api/webhook', express.raw({ type: 'application/json' }), async (reque
       const isAppointment = session.metadata.isAppointments;
 
       if (isQueueing === "true" && isAppointment === "false") {
+        
+        const salon = await getSalonBySalonId(session.metadata.salonId)
+
+        const existingExpiryDate = parseInt(salon.queueingExpiryDate, 10); // Convert stored Unix timestamp to an integer
+        const paymentDaysToAdd = parseInt(session.metadata.paymentExpiryDate, 10); // Number of days to add
+      
+        // Calculate the new expiry date
+        const newExpiryDate = moment.unix(existingExpiryDate) // Convert existing Unix timestamp to a moment object
+          .add(paymentDaysToAdd, 'days') // Add the payment expiry days
+          .unix(); 
 
         const isQueuingValue = Boolean(session.metadata.isQueuing)
-
         await Salon.updateOne(
           { salonId: session.metadata.salonId },
           {
             $set: {
               isQueuing: isQueuingValue,
-              queueingExpiryDate: session.metadata.paymentExpiryDate
+              queueingExpiryDate: newExpiryDate
             },
           }
         )
@@ -243,7 +252,7 @@ app.post('/api/webhook', express.raw({ type: 'application/json' }), async (reque
 
       await salonPayments(paymentData)
 
-      const salon = await getSalonBySalonId(session.metadata.salonId)
+      // const salon = await getSalonBySalonId(session.metadata.salonId)
 
       const emailSubject = ` Payment Confirmation - ${salon.salonName}`;
       const emailBody = `
@@ -347,6 +356,18 @@ app.post('/api/webhook', express.raw({ type: 'application/json' }), async (reque
       }
 
       if (isAppointment === "true" && isQueueing === "false") {
+
+        const salon = await getSalonBySalonId(session.metadata.salonId)
+
+        const existingExpiryDate = parseInt(salon.appointmentExpiryDate, 10); // Convert stored Unix timestamp to an integer
+        const paymentDaysToAdd = parseInt(session.metadata.paymentExpiryDate, 10); // Number of days to add
+      
+        // Calculate the new expiry date
+        const newExpiryDate = moment.unix(existingExpiryDate) // Convert existing Unix timestamp to a moment object
+          .add(paymentDaysToAdd, 'days') // Add the payment expiry days
+          .unix(); 
+
+
         const isAppointmentValue = Boolean(session.metadata.isAppointments)
 
         await Salon.updateOne(
@@ -354,7 +375,7 @@ app.post('/api/webhook', express.raw({ type: 'application/json' }), async (reque
           {
             $set: {
               isAppointments: isAppointmentValue,
-              appointmentExpiryDate: session.metadata.paymentExpiryDate
+              appointmentExpiryDate: newExpiryDate
             },
           }
         )
@@ -371,7 +392,7 @@ app.post('/api/webhook', express.raw({ type: 'application/json' }), async (reque
 
       await salonPayments(paymentData)
 
-      const salon = await getSalonBySalonId(session.metadata.salonId)
+      // const salon = await getSalonBySalonId(session.metadata.salonId)
 
       const emailSubject = ` Payment Confirmation - ${salon.salonName}`;
       const emailBody = `
@@ -475,6 +496,25 @@ app.post('/api/webhook', express.raw({ type: 'application/json' }), async (reque
       }
 
       if (isAppointment === "true" && isQueueing === "true") {
+
+        const salon = await getSalonBySalonId(session.metadata.salonId)
+
+        const existingAppointmentExpiryDate = parseInt(salon.appointmentExpiryDate, 10); // Convert stored Unix timestamp to an integer
+        const existingQueuingExpiryDate = parseInt(salon.queueingExpiryDate, 10); // Convert stored Unix timestamp to an integer
+        const paymentDaysToAdd = parseInt(session.metadata.paymentExpiryDate, 10); // Number of days to add
+      
+        // Calculate the new expiry date
+        const newAppointmentExpiryDate = moment.unix(existingAppointmentExpiryDate) // Convert existing Unix timestamp to a moment object
+          .add(paymentDaysToAdd, 'days') // Add the payment expiry days
+          .unix(); 
+
+            // Calculate the new expiry date
+        const newQueuingExpiryDate = moment.unix(existingQueuingExpiryDate) // Convert existing Unix timestamp to a moment object
+        .add(paymentDaysToAdd, 'days') // Add the payment expiry days
+        .unix(); 
+
+
+
         const isQueuingValue = Boolean(session.metadata.isQueuing)
         const isAppointmentValue = Boolean(session.metadata.isAppointments)
 
@@ -484,11 +524,115 @@ app.post('/api/webhook', express.raw({ type: 'application/json' }), async (reque
             $set: {
               isQueuing: isQueuingValue,
               isAppointments: isAppointmentValue,
-              queueingExpiryDate: session.metadata.paymentExpiryDate,
-              appointmentExpiryDate: session.metadata.paymentExpiryDate
+              queueingExpiryDate: newQueuingExpiryDate,
+              appointmentExpiryDate: newAppointmentExpiryDate
             },
           }
         )
+
+        await salonPayments(paymentData)
+
+        // const salon = await getSalonBySalonId(session.metadata.salonId)
+  
+        const emailSubject = ` Payment Confirmation - ${salon.salonName}`;
+        const emailBody = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Payment Confirmation</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto&display=swap" rel="stylesheet">
+  
+          <style>
+          body {
+                 font-family: 'Poppins', sans-serif;
+                 margin: 0;
+                 padding: 0;
+                 background-color: #f9f9f9;
+                 color: #000,
+              }
+              .container {
+                  max-width: 600px;
+                  margin: 20px auto;
+                  padding: 20px;
+                  background-color: #ffffff;
+                  border-radius: 10px;
+                  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+              }
+              .header {
+                  text-align: center;
+                  margin-bottom: 20px;
+              }
+              .logo img {
+                  max-width: 150px;
+                  border-radius: 50%;
+                  width: 150px;
+                  height: 150px;
+                  object-fit: cover;
+              }
+              .email-content {
+                  padding: 20px;
+                  background-color: #f8f8f8;
+                  font-size: 1rem;
+                  border-radius: 10px;
+              }
+              ul {
+                  padding-left: 20px;
+              }
+              li {
+                  margin-bottom: 8px;
+              }
+              p {
+                  line-height: 1.6;
+              }
+              .footer {
+                  margin-top: 20px;
+                  font-size: 0.9em;
+                  text-align: center;
+                  color: #888888;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <div class="email-content">
+              <div class="header">
+                  <h1>Payment Confirmation</h1>
+              </div>
+                  <p>Dear ${session.customer_details.name},</p>
+                  <p>Thank you for your payment at <strong>${salon.salonName}</strong>. Below are the details of your transaction:</p>
+                  <ul>
+                      <li><strong>Purchase Date:</strong> ${moment.unix(session.metadata.purchaseDate).format('YYYY-MM-DD')}</li>
+                      <li><strong>Expiry Date:</strong> ${moment.unix(session.metadata.paymentExpiryDate).format('YYYY-MM-DD')}</li>
+                      <li><strong>Total Amount Paid:</strong> ${session.currency.toUpperCase()} ${session.amount_total / 100}</li>
+                      <li><strong>Products Purchased:</strong> ${products.map(product => product.name).join(', ')}</li>
+                  </ul>
+                  <p>If you have any questions or need further assistance, feel free to contact us.</p>
+                  <p>Best regards,</p>
+                  <p>
+                      <strong>IQueueBook</strong><br>
+                      <strong>support@iqueuebarbers.com</strong> 
+                  </p>
+              </div>
+              <div class="footer">
+                  &copy; ${new Date().getFullYear()} IQueueBook. All rights reserved.
+              </div>
+          </div>
+      </body>
+      </html>
+      `;
+  
+        try {
+          sendPaymentSuccesEmail(session.customer_details.email, emailSubject, emailBody, invoice, session, products);
+          console.log("Payment Email Sent")
+          return
+        } catch (error) {
+          console.error('Error sending email:', error);
+          return
+        }
 
         // await SalonSettings.updateOne(
         //   { salonId: session.metadata.salonId },
@@ -502,112 +646,6 @@ app.post('/api/webhook', express.raw({ type: 'application/json' }), async (reque
         //   }
         // )
       }
-
-
-      await salonPayments(paymentData)
-
-      const salon = await getSalonBySalonId(session.metadata.salonId)
-
-      const emailSubject = ` Payment Confirmation - ${salon.salonName}`;
-      const emailBody = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Payment Confirmation</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto&display=swap" rel="stylesheet">
-
-        <style>
-        body {
-               font-family: 'Poppins', sans-serif;
-               margin: 0;
-               padding: 0;
-               background-color: #f9f9f9;
-               color: #000,
-            }
-            .container {
-                max-width: 600px;
-                margin: 20px auto;
-                padding: 20px;
-                background-color: #ffffff;
-                border-radius: 10px;
-                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-            }
-            .header {
-                text-align: center;
-                margin-bottom: 20px;
-            }
-            .logo img {
-                max-width: 150px;
-                border-radius: 50%;
-                width: 150px;
-                height: 150px;
-                object-fit: cover;
-            }
-            .email-content {
-                padding: 20px;
-                background-color: #f8f8f8;
-                font-size: 1rem;
-                border-radius: 10px;
-            }
-            ul {
-                padding-left: 20px;
-            }
-            li {
-                margin-bottom: 8px;
-            }
-            p {
-                line-height: 1.6;
-            }
-            .footer {
-                margin-top: 20px;
-                font-size: 0.9em;
-                text-align: center;
-                color: #888888;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="email-content">
-            <div class="header">
-                <h1>Payment Confirmation</h1>
-            </div>
-                <p>Dear ${session.customer_details.name},</p>
-                <p>Thank you for your payment at <strong>${salon.salonName}</strong>. Below are the details of your transaction:</p>
-                <ul>
-                    <li><strong>Purchase Date:</strong> ${moment.unix(session.metadata.purchaseDate).format('YYYY-MM-DD')}</li>
-                    <li><strong>Expiry Date:</strong> ${moment.unix(session.metadata.paymentExpiryDate).format('YYYY-MM-DD')}</li>
-                    <li><strong>Total Amount Paid:</strong> ${session.currency.toUpperCase()} ${session.amount_total / 100}</li>
-                    <li><strong>Products Purchased:</strong> ${products.map(product => product.name).join(', ')}</li>
-                </ul>
-                <p>If you have any questions or need further assistance, feel free to contact us.</p>
-                <p>Best regards,</p>
-                <p>
-                    <strong>IQueueBook</strong><br>
-                    <strong>support@iqueuebarbers.com</strong> 
-                </p>
-            </div>
-            <div class="footer">
-                &copy; ${new Date().getFullYear()} IQueueBook. All rights reserved.
-            </div>
-        </div>
-    </body>
-    </html>
-    `;
-
-      try {
-        sendPaymentSuccesEmail(session.customer_details.email, emailSubject, emailBody, invoice, session, products);
-        console.log("Payment Email Sent")
-        return
-      } catch (error) {
-        console.error('Error sending email:', error);
-        return
-      }
-
     }
   }
   response.status(200).json({ received: true });
@@ -880,7 +918,7 @@ app.post("/api/create-checkout-session", async (req, res) => {
           adminEmail: productInfo.adminEmail,
           purchaseDate: new Date(),
           paymentType: productInfo.paymentType,
-          paymentExpiryDate: expiryDate,
+          paymentExpiryDate: productInfo.paymentExpiryDate,
           isQueuing: productInfo.isQueuing,
           isAppointments: productInfo.isAppointments
         },
