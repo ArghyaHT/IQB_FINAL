182790154dc1ae12afbedb058e8cb8b6dde48756
Arghya211994 <arghya@himanstech.com>
2025-01-21 17:14:58 +0530

    bug check for invoice

--- utils/invoice/invoicepdf.js
+++ utils/invoice/invoicepdf.js
@@ -39,7 +39,7 @@ export const generateInvoicePDF = async (invoice, session, products) => {
   doc.fontSize(10)
     .text(`Invoice #: ${invoice}`, detailsX, detailsY + 15)
     .text(`Invoice Issued: ${moment().format('DD MMM, YYYY')}`, detailsX, detailsY + 30)
-    .text(`Invoice Amount: ${salon.currency}${(session.amount_total / 100).toFixed(2)}`, detailsX, detailsY + 45)
+    .text(`Invoice Amount: ${salon.currency}${(session.metadata.amount_total / 100).toFixed(2)}`, detailsX, detailsY + 45)
   // Print "Status: " without changing color
   doc.text('Status: ', detailsX, detailsY + 60);
 
@@ -47,7 +47,7 @@ export const generateInvoicePDF = async (invoice, session, products) => {
   doc.fillColor('green') // Set color to green
     .fontSize(12) // Increase the font size
     .font('Helvetica-Bold') // Make it bold
-    .text(session.payment_status.toUpperCase(), detailsX+35, detailsY + 60); // Adjust X for proper alignment
+    .text(session.metadata.payment_status.toUpperCase(), detailsX+35, detailsY + 60); // Adjust X for proper alignment
 
   // Reset color to black and font back to default for subsequent text
   doc.fillColor('black')
@@ -61,8 +61,8 @@ export const generateInvoicePDF = async (invoice, session, products) => {
     .lineTo(550, billedToY + 5) // Keep the same Y-coordinate for a straight line
     .stroke();
   doc.fontSize(12).text('BILLED TO', 50, billedToY + 30, { underline: true });
-  doc.fontSize(10).text(session.customer_details.name, 50, billedToY + 50);
-  doc.text(session.customer_details.email, 50, billedToY + 65);
+  doc.fontSize(10).text(session.metadata.customer_details.name, 50, billedToY + 50);
+  doc.text(session.metadata.customer_details.email, 50, billedToY + 65);
 
   // Table Header
   const tableTop = billedToY + 120;
