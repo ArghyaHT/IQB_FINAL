e11b390ab40877bfd3ed469dd2a3871ca0604ca7
Arghya211994 <arghya@himanstech.com>
2025-02-13 11:26:33 +0530

    return empty array if history not found

--- services/mobile/appointmentHistoryService.js
+++ services/mobile/appointmentHistoryService.js
@@ -4,6 +4,10 @@ export const getAppointmentsByCustomerEmail = async(salonId, customerEmail) => {
 
     const getCustomerBookingBySalonId = await AppointmentHistory.findOne({salonId}) 
 
+    if (!getCustomerBookingBySalonId) {
+        return []; // Return null if no record is found
+    }
+
     const filteredAppointmentList = getCustomerBookingBySalonId.appointmentList.filter(item => item.customerEmail === customerEmail);
     
      return filteredAppointmentList
