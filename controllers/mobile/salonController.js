e0fef8eb531e1389a3cf7d6ac07cef23703bef90
Arghya211994 <arghya@himanstech.com>
2025-03-31 17:14:59 +0530

    salonbusinessdays added in saloninfo

--- controllers/mobile/salonController.js
+++ controllers/mobile/salonController.js
@@ -1,6 +1,7 @@
 import { getbarbersBySalonId } from "../../services/mobile/barberService.js";
 import { getAverageSalonRating } from "../../services/mobile/salonRatingService.js";
 import { allSalonServices, allSalons, getAllSalonsByCountry, getSalonRating, salonInfoDetails, searchSalonsByLocation, searchSalonsByNameAndCity } from "../../services/mobile/salonServices.js";
+import { getSalonSettings } from "../../services/mobile/salonSettingsService.js";
 import { findAdminByEmailandRoleTest } from "../../services/web/admin/adminService.js";
 
 ///DESC:GET ALL SALONS =====================
@@ -13,7 +14,7 @@ export const getAllSalons = async (req, res, next) => {
     });
   } catch (error) {
     next(error);
-}
+  }
 }
 
 //DESC:GET ALL SALON SERVICES ======================
@@ -22,7 +23,7 @@ export const getAllSalonServices = async (req, res, next) => {
   try {
     const salonServices = await allSalonServices(salonId)
 
-    if(!salonServices){
+    if (!salonServices) {
       res.status(201).json({
         success: true,
         message: "Salon services not found for the salon.",
@@ -34,7 +35,7 @@ export const getAllSalonServices = async (req, res, next) => {
       message: "Salon services retrieved",
       response: salonServices
     })
-    
+
   }
   catch (error) {
     next(error);
@@ -43,96 +44,118 @@ export const getAllSalonServices = async (req, res, next) => {
 
 //DESC:GET SALON INFO ==================
 export const getSalonInfo = async (req, res, next) => {
-const { salonId } = req.query;
-try {
-  // Find salon information by salonId
-  const salonInfo = await salonInfoDetails(salonId)
+  const { salonId } = req.query;
+  try {
+    // Find salon information by salonId
+    const salonInfo = await salonInfoDetails(salonId)
 
-  if (!salonInfo) {
-    res.status(201).json({
-      success: false,
-      message: 'No salons found for the particular SalonId.',
-    });
-  }
+    if (!salonInfo) {
+      res.status(201).json({
+        success: false,
+        message: 'No salons found for the particular SalonId.',
+      });
+    }
 
-  // Find associated barbers using salonId
-  const barbers = await getbarbersBySalonId(salonId)
+    // Find associated barbers using salonId
+    const barbers = await getbarbersBySalonId(salonId)
 
     const salonRating = await getAverageSalonRating(salonId)
 
-  res.status(200).json({
-    success: true,
-    message: 'Salon and barbers found successfully.',
-    response: {
-      salonInfo: salonInfo,
-      barbers: barbers,
-        salonRating: salonRating
-    },
-  });
-} catch (error) {
-  next(error);
-}
+    const salonSettings = await getSalonSettings(salonId);
+
+    // Define all days of the week
+    const allDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    // Get off days from the salon settings
+    const salonOffDays = salonSettings.salonOffDays || [];
+
+     // Get business hours from salon settings
+     const startTime = salonSettings.appointmentSettings.appointmentStartTime;
+     const endTime = salonSettings.appointmentSettings.appointmentEndTime;
+
+    // Construct the response with timings
+    const businessDays = allDays.map(day => {
+        if (salonOffDays.includes(day)) {
+            return { day, "status": "closed" };
+        }
+        return { day, startTime, endTime };
+    });
+
+
+    res.status(200).json({
+      success: true,
+      message: 'Salon and barbers found successfully.',
+      response: {
+        salonInfo: salonInfo,
+        barbers: barbers,
+        salonRating: salonRating,
+        salonBusinessDays: businessDays
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 
 //SEARCH SALONS BY NAME AND CITY
 export const getSalonsByNameAndCity = async (req, res, next) => {
-try {
-  const { searchValue, limit = 10, sortField, sortOrder } = req.query;
+  try {
+    const { searchValue, limit = 10, sortField, sortOrder } = req.query;
 
-  let query = {};
+    let query = {};
 
-  //Creating the RegExp For salonName and City
-  const searchRegExpName = new RegExp('.*' + searchValue + ".*", 'i')
-  const searchRegExpCity = new RegExp('.*' + searchValue + ".*", 'i')
+    //Creating the RegExp For salonName and City
+    const searchRegExpName = new RegExp('.*' + searchValue + ".*", 'i')
+    const searchRegExpCity = new RegExp('.*' + searchValue + ".*", 'i')
 
-  //Query for searching salonName and City
-  if (searchValue) {
-    query.$or = [
-      { salonName: { $regex: searchRegExpName } },
-      { city: { $regex: searchRegExpCity } }
-    ];
+    //Query for searching salonName and City
+    if (searchValue) {
+      query.$or = [
+        { salonName: { $regex: searchRegExpName } },
+        { city: { $regex: searchRegExpCity } }
+      ];
+    }
+
+    const sortOptions = {};
+
+    //Creating sorting options
+    if (sortField && sortOrder) {
+      sortOptions[sortField] = sortOrder === 'asc' ? 1 : -1;
+    }
+
+    const getAllSalons = await searchSalonsByNameAndCity(query, sortOptions, limit);
+    res.status(200).json({
+      success: true,
+      message: "Salons retrieved successsfully",
+      response: getAllSalons,
+    })
+  } catch (error) {
+    next(error);
   }
-
-  const sortOptions = {};
-
-  //Creating sorting options
-  if (sortField && sortOrder) {
-    sortOptions[sortField] = sortOrder === 'asc' ? 1 : -1;
-  }
-
-  const getAllSalons = await searchSalonsByNameAndCity(query, sortOptions, limit);
-  res.status(200).json({
-    success: true,
-    message: "Salons retrieved successsfully",
-    response: getAllSalons,
-  })
-} catch (error) {
-  next(error);
-}
 }
 
 
 //SEARCH SALONS BY LOCATION
 export const getSalonsByLocation = async (req, res, next) => {
 
-try {
-  const { longitude, latitude } = req.query;
-  let salons = [];
-  salons = await searchSalonsByLocation(longitude, latitude)
+  try {
+    const { longitude, latitude } = req.query;
+    let salons = [];
+    salons = await searchSalonsByLocation(longitude, latitude)
 
-   // Populate salonRatings field
-   await getSalonRating(salons);
+    // Populate salonRatings field
+    await getSalonRating(salons);
 
-  return res.status(200).json({
-    success: true,
-    message: "Salons retrieved successfully",
-    response: salons
-  });
-}
-catch (error) {
-  next(error);
-}
+    return res.status(200).json({
+      success: true,
+      message: "Salons retrieved successfully",
+      response: salons
+    });
+  }
+  catch (error) {
+    next(error);
+  }
 }
 
 
@@ -146,7 +169,7 @@ export const getallSalonsTest = async (req, res, next) => {
       adminEmail: salon.adminEmail,
       salonName: salon.salonName,
       currency: salon.currency,
-      isoCurrencyCode:salon.isoCurrencyCode
+      isoCurrencyCode: salon.isoCurrencyCode
     }));
 
     return res.status(200).json({
@@ -160,10 +183,10 @@ export const getallSalonsTest = async (req, res, next) => {
 };
 
 
-export const getAdminDetailsByAdminEmailTest = async(req, res, next) => {
-  try{
+export const getAdminDetailsByAdminEmailTest = async (req, res, next) => {
+  try {
 
-    const {adminEmail} = req.body;
+    const { adminEmail } = req.body;
 
     const admin = await findAdminByEmailandRoleTest(adminEmail)
 
@@ -177,26 +200,26 @@ export const getAdminDetailsByAdminEmailTest = async(req, res, next) => {
   }
   catch (error) {
     next(error);
-}
+  }
 }
 
 //GET ALL SALON CITIES
-  export const getSalonCities = async(req, res, next) => {
-    try{
-      const {country} = req.body;
+export const getSalonCities = async (req, res, next) => {
+  try {
+    const { country } = req.body;
 
-      const salonsByCountry = await getAllSalonsByCountry(country)
+    const salonsByCountry = await getAllSalonsByCountry(country)
 
-      const cities = [...new Set(salonsByCountry.map(salon => salon.city))];
+    const cities = [...new Set(salonsByCountry.map(salon => salon.city))];
 
-      return res.status(200).json({ 
-        success: true,
-        message: "All salon cities retrieved successfully",
-        response: cities 
-      });
+    return res.status(200).json({
+      success: true,
+      message: "All salon cities retrieved successfully",
+      response: cities
+    });
 
-    }
-    catch (error) {
-      next(error);
   }
+  catch (error) {
+    next(error);
   }
+}
