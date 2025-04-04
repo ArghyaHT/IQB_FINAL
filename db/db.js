bbfbd9132edbe3804cab816faaefcf02c0626a81
Arghya211994 <arghya@himanstech.com>
2025-04-04 15:02:56 +0530

    db uri changed

--- db/db.js
+++ db/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async() =>{
     try{
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
+        const conn = await mongoose.connect("mongodb+srv://IQB:IQB@iqb.ilmxir5.mongodb.net/IQB")
 
         console.log(`MongoDB is successfully connected: ${conn.connection.host}`)
     }
