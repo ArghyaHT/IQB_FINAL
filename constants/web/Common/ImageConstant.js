677fce5d68f2e6b456042c1b02d96f25a0e75959
Arghya211994 <arghya@himanstech.com>
2025-05-07 16:18:43 +0530

    total limit for image upto 20 mb

--- constants/web/Common/ImageConstant.js
+++ constants/web/Common/ImageConstant.js
@@ -3,6 +3,6 @@ export const ALLOWED_IMAGE_EXTENSIONS = ["jpg", "png", "jpeg", "webp"]
 export const IMAGE_FAILED_DELETE = "Failed to delete temporary file"
 export const BARBER_IMAGE_EMPTY_ERROR = "Barber profile not present"
 
-export const TOTAL_IMAGE_SIZE_ERROR = "Total image size exceeds 3MB limit"
+export const TOTAL_IMAGE_SIZE_ERROR = "Total image size exceeds 20MB limit"
 
-export const TOTAL_IMAGE_UPLOAD_SIZE = 3 * 1024 * 1024
+export const TOTAL_IMAGE_UPLOAD_SIZE = 20 * 1024 * 1024
