2ec3241911989ef2dbe40ae2c327973d85759e24
Arghya211994 <arghya@himanstech.com>
2025-06-12 17:54:05 +0530

    pro pic updated  for queueupdated  when coming from kiosk

--- services/kiosk/queue/queueService.js
+++ services/kiosk/queue/queueService.js
@@ -1,52 +1,62 @@
 import SalonQueueList from "../../../models/salonQueueListModel.js";
+import { findCustomerByEmail } from "../../web/customer/customerService.js";
 import { getBarberByBarberId } from "../barber/barberService.js";
 
 
 // Find existing SalonQueueList
-export const findSalonQueueList = async(salonId) => {
+export const findSalonQueueList = async (salonId) => {
   const queueList = await SalonQueueList.findOne({ salonId: salonId });
-  
+
   return queueList;
-  }
+}
 
 //Adding new queue document
-export const addNewQueue = async(salonId, newQueue) => {
+export const addNewQueue = async (salonId, newQueue) => {
   const newQueueData = new SalonQueueList({
-      salonId: salonId,
-      queueList: [newQueue],
-    });
-   await newQueueData.save();
-   return newQueueData;
+    salonId: salonId,
+    queueList: [newQueue],
+  });
+  await newQueueData.save();
+  return newQueueData;
 }
 
 //Get salon queue list
-export const getSalonQlist = async(salonId) => {
+export const getSalonQlist = async (salonId) => {
+
+  const defaultProfileImage = [
+    {
+      url: "https://res.cloudinary.com/dpynxkjfq/image/upload/v1720520065/default-avatar-icon-of-social-media-user-vector_wl5pm0.jpg",
+    },
+  ];
 
   const sortedQlist = await SalonQueueList.findOne({ salonId }).lean();
 
   if (!sortedQlist || !sortedQlist.queueList || sortedQlist.queueList.length === 0) {
     return [];
-}
+  }
 
   const updatedQueueList = await Promise.all(
     sortedQlist.queueList.map(async (queue) => {
-        const barber = await getBarberByBarberId(queue.barberId);
-        return {
-            ...queue, // Spread the existing queue properties
-            barberEmail: barber ? barber.email : null, // Add barberEmail
-            name: queue.customerName
-        };
+      const barber = await getBarberByBarberId(queue.barberId);
+      const customer = await findCustomerByEmail(queue.customerEmail)
+      return {
+        ...queue, // Spread the existing queue properties
+        barberEmail: barber ? barber.email : null, // Add barberEmail
+        barberProfile: barber ? barber.profile : defaultProfileImage,
+        customerProfile: customer ? customer.profile : defaultProfileImage,
+        name: queue.customerName
+      };
     })
-);
+  );
 
 
-return updatedQueueList;
+  return updatedQueueList;
 
 }
 
 
 //Find customers to send Mail for Q position change
-export const findCustomersToMail = async(salonId, barberId) => {
+export const findCustomersToMail = async (salonId, barberId) => {
   const customers = await SalonQueueList.find({ salonId, "queueList.barberId": barberId })
 
   return customers;
@@ -55,7 +65,7 @@ export const findCustomersToMail = async(salonId, barberId) => {
 
 
 //GET Q LIST BY BARBER ID
-export const qListByBarberId = async(salonId, barberId) => {
+export const qListByBarberId = async (salonId, barberId) => {
   const qList = await SalonQueueList.aggregate([
     {
       $match: {
